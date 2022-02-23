71c8112a08a0a0e5e8cff71e764bf56733cfcbe9
lavnisoumya <100011175+lavnisoumya@users.noreply.github.com>
2022-02-23 18:14:30 +0530

    feb23

--- src/app/sixthscreen/sixthscreen.component.ts
+++ src/app/sixthscreen/sixthscreen.component.ts
@@ -6,7 +6,13 @@ import { Router } from "@angular/router";
   styleUrls: ['./sixthscreen.component.css']
 })
 export class SixthscreenComponent implements OnInit {
-
+   isfirstclicked = false;
+   issecondclicked = false;
+   isthirdclicked = false;
+   isfirstdisabled = false;
+   isseconddisabled = false;
+   isthirddisabled = false;
+   isproceeddisabled = true;
   constructor(private route: Router) { }
 
   ngOnInit(): void {
@@ -23,4 +29,41 @@ export class SixthscreenComponent implements OnInit {
   tittok(){
     this.route.navigate(['/tiktokscreen']);
   }
+  one(){
+    if (this.isfirstclicked = true) {
+      this.isseconddisabled = true;
+       this.isthirddisabled = true;
+
+    };
+  }
+    two(){
+      if(this.issecondclicked = true){
+        this.isfirstdisabled = true;
+        this.isthirddisabled = true;
+      };
+    }
+      three(){
+        if (this.isthirdclicked = true) {
+          this.isseconddisabled = true;
+          this.isfirstdisabled = true ;
+        };
+      }
+
+
+
+//  buttonactive(){
+  //  var btnContainer = document.getElementById("studentactivebutton")!;
+
+// Get all buttons with class="btn" inside the container
+//var btns = btnContainer.getElementsByClassName("button")!;
+
+// Loop through the buttons and add the active class to the current/clicked button
+//for (let index = 0; index < btns.length; index++) {
+//  var cureent = document.getElementsByClassName("active");
+//  cureent[0].className = cureent[0].className.replace("active","");
+//  this.className += "active";
+
+//}
+
+//}
 }
