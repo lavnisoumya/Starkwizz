eba55736de12a0aeacdf09052311c023ab5dd6b1
lavnisoumya <100011175+lavnisoumya@users.noreply.github.com>
2022-03-03 13:24:53 +0530

    mar 3

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -25,13 +25,8 @@ playpause(){
   const videos = document.querySelectorAll('video')!;
 
        for (const video of videos) {
-         video.addEventListener('click', function () {
-           if (video.paused) {
-             video.play();
-           } else {
-             video.pause();
-           }
-         });
+         video.play();
+        
        }
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false
+  
 };
 
 /*
